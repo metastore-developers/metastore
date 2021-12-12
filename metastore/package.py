be347d384c7ee64651998892d3fd448630ea378f
danilopeixoto <danilopeixoto@outlook.com>
2021-12-12 13:14:39 -0300

    Fix image URL

--- metastore/package.py
+++ metastore/package.py
@@ -6,7 +6,7 @@ Package description information.
 __title__ = 'Metastore'
 
 #: Package version.
-__version__ = '1.0.0.dev7'
+__version__ = '1.0.0.dev8'
 
 #: Package description.
 __description__ = 'Metastore Python SDK.'
