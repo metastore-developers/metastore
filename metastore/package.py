4a3bb6ab1f858eb4f1b8e190cf5c54e9135f50eb
danilopeixoto <danilopeixoto@outlook.com>
2021-12-26 20:34:48 -0300

    Add development and production environments

--- metastore/package.py
+++ metastore/package.py
@@ -6,7 +6,7 @@ Package description information.
 __title__ = 'Metastore'
 
 #: Package version.
-__version__ = '1.0.0.dev20'
+__version__ = '1.0.0.dev21'
 
 #: Package description.
 __description__ = 'Metastore Python SDK. Feature store and data catalog for machine learning.'
